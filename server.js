1b7fd0a90a8458de8b1a18eda4ce7e03d1b651fd
zombieteer <aryan.nigam1996@gmail.com>
2020-05-22 17:06:16 +0530

    lets see

--- server.js
+++ server.js
@@ -6,16 +6,16 @@ var router = express.Router();
 var cors = require("cors");
 const path = require("path");
 
-app.use(cors());
+// app.use(cors());
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
 
 // app.get("/", (req, res) => res.send({ msg: "welcome" }));
 // Serve static assests if in production
@@ -23,7 +23,7 @@ if (process.env.NODE_ENV === "production") {
   // set static folder
   app.use(express.static("client/build"));
   app.get("*", (req, res) => {
-    res.sendfile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
