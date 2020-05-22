682a96ddab155152e4e333a4b6c4c67a60465ee0
zombieteer <aryan.nigam1996@gmail.com>
2020-05-22 17:16:08 +0530

    asd

--- server.js
+++ server.js
@@ -23,6 +23,8 @@ if (process.env.NODE_ENV === "production") {
   // set static folder
   app.use(express.static("client/build"));
   app.get("*", (req, res) => {
+    console.log("other way round");
+
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
