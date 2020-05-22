d0aa5d2d1b138b8f0ac4945c79d382652c26f641
zombieteer <aryan.nigam1996@gmail.com>
2020-05-22 17:28:28 +0530

    asda

--- server.js
+++ server.js
@@ -22,11 +22,9 @@ const path = require("path");
 if (process.env.NODE_ENV === "production") {
   // set static folder
   app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    console.log("other way round");
-
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
 }
 
 const PORT = process.env.PORT || 5000;
@@ -69,10 +67,12 @@ app.post("/", (req, res) => {
 
   transporter.sendMail(mail, (err, data) => {
     if (err) {
+      console.log("message hasn't been send");
       res.json({
         status: "fail",
       });
     } else {
+      console.log("message send");
       res.json({
         status: "success",
       });
