443e640f0d0114a1779393455f671325a12bd816
zombieteer <aryan.nigam1996@gmail.com>
2020-05-22 11:28:23 +0530

    port changed

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const PORT = process.env.PORT || 5661;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server Started at ${PORT}`));
 
 let transporter = nodemailer.createTransport({
