fd6e4fcbb1b2e235bb685620f147302fb8713964
zombieteer <aryan.nigam1996@gmail.com>
2020-05-23 21:19:41 +0530

    post

--- server.js
+++ server.js
@@ -7,28 +7,18 @@ var cors = require("cors");
 const path = require("path");
 
 // app.use(cors());
+app.use(express.json({ extended: false }));
 
-// app.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// });
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 // app.get("/", (req, res) => res.send({ msg: "welcome" }));
-// Serve static assests if in production
-if (process.env.NODE_ENV === "production") {
-  // set static folder
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-  );
-}
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server Started at ${PORT}`));
 
 let transporter = nodemailer.createTransport({
   service: "gmail",
@@ -49,8 +39,6 @@ transporter.verify((error, success) => {
     console.log("Server is ready to take messages");
   }
 });
-
-app.use(express.json());
 
 app.post("/", (req, res) => {
   console.log("into the post request");
@@ -80,3 +68,15 @@ app.post("/", (req, res) => {
     }
   });
 });
+
+// Serve static assests if in production
+if (process.env.NODE_ENV === "production") {
+  // set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server Started at ${PORT}`));
